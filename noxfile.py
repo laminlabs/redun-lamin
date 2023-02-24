f9cae8c0543806019cc496030cdee44678f95658
Alex Wolf <f.alexander.wolf@gmail.com>
2023-02-24 22:02:27 +0100

    :construction_worker: Update CI

--- noxfile.py
+++ noxfile.py
@@ -1,16 +1,20 @@
 import nox
-from lndb_setup.test.nox import build_docs, run_pre_commit, run_pytest
+from laminci import move_built_docs_to_docs_slash_project_slug, upload_docs_dir
+from laminci.nox import build_docs, login_testuser1, run_pre_commit, run_pytest
 
 nox.options.reuse_existing_virtualenvs = True
 
 
-@nox.session
+@nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
 def lint(session: nox.Session) -> None:
     run_pre_commit(session)
 
 
 @nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
 def build(session):
+    login_testuser1(session)
     session.install(".[dev,test]")
-    run_pytest(session)
+    run_pytest(session, coverage=False)
     build_docs(session)
+    upload_docs_dir()
+    move_built_docs_to_docs_slash_project_slug()
