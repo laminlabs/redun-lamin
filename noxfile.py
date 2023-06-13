03db28c0a1f8df48f4d7476e380fc4f9c36381d2
Alex Wolf <f.alexander.wolf@gmail.com>
2023-06-13 17:28:23 +0200

    :construction_worker: Refactor CI

--- noxfile.py
+++ noxfile.py
@@ -1,20 +1,24 @@
 import nox
-from laminci import move_built_docs_to_docs_slash_project_slug, upload_docs_artifact
+from laminci import upload_docs_artifact
 from laminci.nox import build_docs, login_testuser1, run_pre_commit, run_pytest
 
-nox.options.reuse_existing_virtualenvs = True
+nox.options.default_venv_backend = "none"
 
 
-@nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
+@nox.session
 def lint(session: nox.Session) -> None:
     run_pre_commit(session)
+
+
+@nox.session
+def install(session: nox.Session):
+    session.run(*"pip install .[dev]".split())
+    session.run("pip", "install", "lamindb @ git+https://github.com/laminlabs/lamindb")
 
 
 @nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
 def build(session):
     login_testuser1(session)
-    session.install(".[dev,test]")
     run_pytest(session, coverage=False)
     build_docs(session)
     upload_docs_artifact()
-    move_built_docs_to_docs_slash_project_slug()
