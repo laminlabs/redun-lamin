b3996230226701806eaf91d1d8299400b80eae1d
zethson <lukas.heumos@posteo.net>
2025-01-08 10:56:42 +0100

    Ruff
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,6 @@
 import nox
 from laminci import upload_docs_artifact
-from laminci.nox import build_docs, login_testuser1, run_pre_commit, run_pytest
+from laminci.nox import build_docs, login_testuser1, run_pre_commit, run_pytest, install_lamindb
 
 nox.options.default_venv_backend = "none"
 
@@ -13,14 +13,7 @@ def lint(session: nox.Session) -> None:
 @nox.session
 def install(session: nox.Session):
     session.run(*"uv pip install --system .[dev]".split())
-    session.run(
-        "uv",
-        "pip",
-        "install",
-        "--system",
-        "lamindb[bionty] @ git+https://github.com/laminlabs/lamindb@main",
-    )
-
+    install_lamindb(session, branch="release", extras="bionty")
 
 @nox.session
 def build(session):
